8e20fa7a32054f0e070e2ae791b0ad8ffbe12a98
Muskan Pasricha <100332850+Muk200@users.noreply.github.com>
2023-04-02 23:39:07 +0530

    Add files via upload

---
+++ Patterns/Pattern4.cpp
@@ -0,0 +1,23 @@
+//  TO PRINT
+    //   1 2 3
+    //   4 5 6
+    //   7 8 9
+
+    int n;
+    cin>>n;
+    int row = 1;
+    int count = 1;
+
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= n)
+        {
+            cout << count<< " ";
+            count = count + 1;
+            col = col + 1; 
+        } 
+        cout<<endl;
+
+        row = row + 1;   
+    }
