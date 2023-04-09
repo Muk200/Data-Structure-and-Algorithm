a4274f53be2a1fb339eff4f737e6b1b9fcbc1b03
Muskan Pasricha <100332850+Muk200@users.noreply.github.com>
2023-04-09 07:10:48 +0530

    Add files via upload

---
+++ Patterns/Pattern25.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    // TO PRINT 
+    // 1 1 1 1
+    // 2 2 2
+    // 3 3 
+    // 4
+
+    int n;
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int col = 1;
+        while(col <= n-row+1) {
+            cout << row << " ";
+            col = col+1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+    return 0;
+}
